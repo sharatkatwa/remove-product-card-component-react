ca6fef0d76a21795e0a6e26d29131da37374c2b7
sharatkatwa <sjkatwa2000@gmail.com>
2026-03-15 22:55:41 +0530

    add transition on hover cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,7 +2,7 @@ import { HiTrash } from "react-icons/hi2";
 
 function ProductCard({details,removeProduct}) {
   return (
-    <div className='p-1 bg-white rounded-2xl h-fit border border-gray-100 shadow'>
+    <div className='p-1 bg-white rounded-2xl h-fit border border-gray-100 shadow hover:-translate-y-1 transition-all duration-200 ease-out'>
       <div className="w-100 h-[400px] overflow-hidden bg-gray-100 rounded-2xl flex item-center justify-center"><img className="object-contain" src={details.image} alt="product image" /></div>
       <div className="flex items-center justify-between my-5 mx-2 h-20">
         <div className="w-70">
